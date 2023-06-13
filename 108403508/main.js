176ec082cab78d0db54d14f1c9e8880f4ee34f15
fangyucha <fangyu@g.ncu.edu.tw>
2023-06-13 22:36:53 +0800

    add type button

--- 108403508/main.js
+++ 108403508/main.js
@@ -2,8 +2,14 @@
 function type(d) {
   return {
     Country: d['Country Name'],
-    MonthlyWagesUSD: parseFloat(d['Real Median Monthly Wages in USD (base 2011), PPP adjusted'])*12,
+    Wage: parseFloat(d['Real Median Monthly Wages in USD (base 2011), PPP adjusted'])*12,
+    AgricultureWage: parseFloat(d['Median Earnings for wage workers per month in agriculture, local nominal currenc']) * 12 * parseFloat(d['Real Median Monthly Wages in USD (base 2011), PPP adjusted']) / parseFloat(d['Median Earnings for wage workers per month, local nominal currency']),
+    IndustryWage: parseFloat(d['Median Earnings for wage workers per month in industry, local nominal currency']) * 12 * parseFloat(d['Real Median Monthly Wages in USD (base 2011), PPP adjusted']) / parseFloat(d['Median Earnings for wage workers per month, local nominal currency']),
+    ServiceWage: parseFloat(d['Median Earnings for wage workers per month in service, local nominal currency']) * 12 * parseFloat(d['Real Median Monthly Wages in USD (base 2011), PPP adjusted']) / parseFloat(d['Median Earnings for wage workers per month, local nominal currency']),
+    WorkingHours: parseFloat(d['Average weekly working hours']),
+    ExcessiveWorkingRatio: parseFloat(d['Excessive working hours,>48 hours per week']),
     WageGap: parseFloat(d['Female Labor Force Participation Rate, aged 15-64']),
+    //PubTOPriWageGap: parseFloat(d['Public to Private wage gap, calculated with median wages']), //資料太少
   }
 }
 
@@ -12,7 +18,8 @@ function filterData(data) {
   return data.filter(
     d => {
       return (
-        d.MonthlyWagesUSD &&
+        d.Wage && d.AgricultureWage && d.IndustryWage && d.ServiceWage &&
+        d.WorkingHours && d.ExcessiveWorkingRatio &&
         d.WageGap
       )
     }
@@ -21,25 +28,31 @@ function filterData(data) {
 
 // prepare data
 function chooseData(metric, dataClean) {
-  const thisData = dataClean.sort((a, b) => b[metric] - a[metric]).filter((d, i) => i < 10)
-  // 取Top 10
+  const thisData = dataClean.sort((a, b) => b[metric] - a[metric]).filter((d, i) => i < 11 && i > 0)
+  // 取Top 10 // 第一名原本是Suriname，但太極端，所以不要
   return thisData
 }
 
 // setup Bar Chart
 function setupCanvas(barChartData, dataClean) {
-  let metric = 'MonthlyWagesUSD' //以收入多寡為預設
+  let metric = 'Wage' //以收入多寡為預設
+  let type = 'Wage' //以收入多寡為預設
 
   function click() {
-    metric = this.dataset.name
+    metric = this.id
+    type = this.dataset.name
     const thisData = chooseData(metric, dataClean)
     update(thisData)
   }
-
+  
   d3.selectAll('button').on('click', click)
 
   function update(data) {
     console.log(data)
+    
+    // update button
+    setButton(type)
+    d3.selectAll('button').on('click', click)
     // update scale
     xMax = d3.max(data, d => d[metric])
     xScale_v3 = d3.scaleLinear([0, xMax], [0, chart_width])
@@ -57,8 +70,25 @@ function setupCanvas(barChartData, dataClean) {
     yAxisDraw.transition(transitionDelay).call(yAxis.scale(yScale))
 
     // update header
-    header.select('tspan').text(`Top 10 ${metric === 'MonthlyWagesUSD' ? '人均年收入國家' : '男女薪資平等國家'} ${metric === 'MonthlyWagesUSD' ? '(美元現值)' : ''}`)
+    if(type === 'Wage') {
+      header.select('tspan').text(`Top 10 人均年收入國家(美元現值)`)
+      if(metric === 'AgricultureWage') {
+        header.select('tspan').text(`Top 10 農業 人均年收入國家(美元現值)`)
+      }else if(metric === 'IndustryWage') {
+        header.select('tspan').text(`Top 10 工業 人均年收入國家(美元現值)`)
+      }else if(metric === 'ServiceWage') {
+        header.select('tspan').text(`Top 10 服務業 人均年收入國家(美元現值)`)
+      }
+    } else if(type === 'WorkingHours') {
+      header.select('tspan').text(`Top 10 平均每週工時國家(小時)`)
+      if(metric === 'ExcessiveWorkingRatio') {
+        header.select('tspan').text(`Top 10 工作超時比例國家(%)`)
+      }
+    } else{
+      header.select('tspan').text(`Top 10 女/男性工資比例國家(%)`)
 
+    }
+   
     // update bar
     bars.selectAll('.bar').data(data, d => d.Country).join(
       enter => {
@@ -104,9 +134,9 @@ function setupCanvas(barChartData, dataClean) {
     .attr('xFormat', '.2f')
 
   //scale
-  const xExtent = d3.extent(barChartData, d => d.MonthlyWagesUSD)
+  const xExtent = d3.extent(barChartData, d => d.Wage)
   //V3.Short writing for v2
-  let xMax = d3.max(barChartData, d => d.MonthlyWagesUSD)
+  let xMax = d3.max(barChartData, d => d.Wage)
   let xScale_v3 = d3.scaleLinear([0, xMax], [0, chart_width])
   //垂直空間的分配 - 平均分布給Top 10
   let yScale = d3.scaleBand().domain(barChartData.map(d => d.Country))
@@ -124,9 +154,11 @@ function setupCanvas(barChartData, dataClean) {
   //tickSizeInner: the length of the tick lines
   //tickSizeOuter: the length of the square ends of the fomain path
   let xAxis = d3.axisTop(xScale_v3)
-    //.tickFormat(formatTicks)
+    .tickFormat(formatTicks)
     .tickSizeInner(-chart_height)
     .tickSizeOuter(0)
+  
+
   let xAxisDraw = this_svg.append('g')
     .attr('class', 'x axis')
 
@@ -142,8 +174,13 @@ function setupCanvas(barChartData, dataClean) {
     // get data
     const thisBarData = d3.select(this).data()[0]
     const bodyData = [
-      ['月收入', formatTicks(thisBarData.MonthlyWagesUSD)],
-      ['女/男性薪資比例', thisBarData.WageGap]
+      ['年收入', 'US$'+formatTicks(thisBarData.Wage)],
+      ['農業收入', 'US$' + formatTicks(thisBarData.AgricultureWage)],
+      ['工業收入', 'US$'+formatTicks(thisBarData.IndustryWage)],
+      ['服務業收入', 'US$'+formatTicks(thisBarData.ServiceWage)],
+      ['每週工時', formatTicks(thisBarData.WorkingHours) + '小時'],
+      ['工作超時比例', formatTicks(thisBarData.ExcessiveWorkingRatio)],
+      ['女/男性薪資比例', formatTicks(thisBarData.WageGap)]
     ]
 
     tip.style('left', (e.clientX + 15)+'px')
@@ -152,7 +189,7 @@ function setupCanvas(barChartData, dataClean) {
         .style('opacity', 0.98)
     
     // html fill
-    tip.select('h3').html(`${thisBarData.Country}`)
+    tip.select('h2').html(`${thisBarData.Country}`)
     d3.select('.tip-body').selectAll('p').data(bodyData)
       .join('p').attr('class', 'top-info')
       .html(d=>`${d[0]} : ${d[1]}`)
@@ -172,41 +209,48 @@ function setupCanvas(barChartData, dataClean) {
     .on('mousemove', mousemove)
     .on('mouseout', mouseout)
 }
-
-// WageGap bar chart
-function setWageGapBarChart(barChartData, dataClean) {
-  const svg_width = 600
-  const svg_height = 500
-  const chart_margin = { top: 80, right: 40, bottom: 40, left: 120 }
-  const chart_width = svg_width - (chart_margin.left + chart_margin.right)
-  const chart_height = svg_height - (chart_margin.top + chart_margin.bottom)
-
-  const this_svg = d3.select('.bar-chart-container').append('svg')
-    .attr('width', svg_width).attr('height', svg_height)
-    .append('g')
-    .attr('transform', `translate(${chart_margin.left}, ${chart_margin.top})`)
-    .attr('xFormat', '.2f')
-
-  //scale
-  const x = d3.scaleBand()
-    .domain(['男性', '女性'])
-    .rangeRound([0, chart_width])
-    .paddingInner(0.25)
-  
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(barChartData, d => d.WageGap)])
-    .range([chart_height, 0])
-  
-
-  
+let btns_div = document.querySelector('.types')
+// set type bytton
+function setButton(metric) {
+  btns_div.childNodes.forEach(e => e.remove())
+  btns_div.childNodes.forEach(e => e.remove())
+  if(metric === 'Wage'){
+    let btn1 = document.createElement('button')
+    btn1.setAttribute('data-name', 'Wage')
+    btn1.setAttribute('id', 'AgricultureWage')
+    let btn2 = document.createElement('button')
+    btn2.setAttribute('data-name', 'Wage')
+    btn2.setAttribute('id', 'IndustryWage')
+    let btn3 = document.createElement('button')
+    btn3.setAttribute('data-name', 'Wage')
+    btn3.setAttribute('id', 'ServiceWage')
+    btn1.innerText = '農業'
+    btn2.innerText = '工業'
+    btn3.innerText = '服務業'
+    btns_div.appendChild(btn1)
+    btns_div.appendChild(btn2)
+    btns_div.appendChild(btn3)
+  }else if(metric === 'WorkingHours'){
+    let btn1 = document.createElement('button')
+    btn1.setAttribute('data-name', 'WorkingHours')
+    btn1.setAttribute('id', 'ExcessiveWorkingRatio')
+    btn1.innerText = '超時工作比例'
+    btns_div.appendChild(btn1)
+  }
 }
 
 // 刻度顯示格式轉換
 function formatTicks(d) {
-  return d3.format('.2s')(d)
-    .replace('M', 'mil')
-    .replace('G', 'bil')
-    .replace('T', 'tri')
+  if(d === 0){
+    return d
+  }else if(d < 1){
+    return d3.format('.0%')(d)
+  }else{
+    return d3.format('.2s')(d)
+      .replace('M', 'mil')
+      .replace('G', 'bil')
+      .replace('T', 'tri')
+  }
 }
 
 //項目名稱太長
@@ -219,7 +263,7 @@ function ready(data) {
   const dataClean = filterData(data)
   console.log(dataClean)
   // get Top 10 wage country
-  const wageData = chooseData("MonthlyWagesUSD", dataClean)
+  const wageData = chooseData("Wage", dataClean)
 
   setupCanvas(wageData, dataClean)
 }
